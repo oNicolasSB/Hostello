69292bcf30a659a22096ab7f6f9f46f57a46335b
Nícolas Bassini <nicolasbassini@hotmail.com>
2022-07-28 15:43:29 -0300

    adição script para avaliação com estrelas

---
+++ script/script_avaliar.js
@@ -0,0 +1,45 @@
+let imgstr1 = document.getElementById("estrela1")
+let imgstr2 = document.getElementById("estrela2")
+let imgstr3 = document.getElementById("estrela3")
+let imgstr4 = document.getElementById("estrela4")
+let imgstr5 = document.getElementById("estrela5")
+
+function avaliar1(){
+    imgstr1.src = "imagens/estrela_amarela.png"
+    imgstr2.src = "imagens/estrela_cinza.png"
+    imgstr3.src = "imagens/estrela_cinza.png"
+    imgstr4.src = "imagens/estrela_cinza.png"
+    imgstr5.src = "imagens/estrela_cinza.png"
+}
+
+function avaliar2(){
+    imgstr1.src = "imagens/estrela_amarela.png"
+    imgstr2.src = "imagens/estrela_amarela.png"
+    imgstr3.src = "imagens/estrela_cinza.png"
+    imgstr4.src = "imagens/estrela_cinza.png"
+    imgstr5.src = "imagens/estrela_cinza.png"
+}
+
+function avaliar3(){
+    imgstr1.src = "imagens/estrela_amarela.png"
+    imgstr2.src = "imagens/estrela_amarela.png"
+    imgstr3.src = "imagens/estrela_amarela.png"
+    imgstr4.src = "imagens/estrela_cinza.png"
+    imgstr5.src = "imagens/estrela_cinza.png"
+}
+
+function avaliar4(){
+    imgstr1.src = "imagens/estrela_amarela.png"
+    imgstr2.src = "imagens/estrela_amarela.png"
+    imgstr3.src = "imagens/estrela_amarela.png"
+    imgstr4.src = "imagens/estrela_amarela.png"
+    imgstr5.src = "imagens/estrela_cinza.png"
+}
+
+function avaliar5(){
+    imgstr1.src = "imagens/estrela_amarela.png"
+    imgstr2.src = "imagens/estrela_amarela.png"
+    imgstr3.src = "imagens/estrela_amarela.png"
+    imgstr4.src = "imagens/estrela_amarela.png"
+    imgstr5.src = "imagens/estrela_amarela.png"
+}
