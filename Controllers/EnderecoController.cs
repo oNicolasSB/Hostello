b7d7b67f02fc8b109cba48fbc199904c98377dee
Nícolas Bassini <nicolasbassini@hotmail.com>
2022-09-28 17:32:49 -0300

    crud endereco

--- Controllers/EnderecoController.cs
+++ Controllers/EnderecoController.cs
@@ -1,4 +1,5 @@
 using hostello.Data;
+using hostello.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hostello.Controllers;
@@ -16,5 +17,66 @@ public class EnderecoController : Controller
     {
         var enderecos = _db.Enderecos.ToList();
         return View(enderecos);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        var endereco = new Endereco();
+        return View(endereco);
+    }
+    [HttpPost]
+    public IActionResult Create(Endereco endereco)
+    {
+        if(!ModelState.IsValid) return View(endereco);
+        _db.Enderecos.Add(endereco);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var endereco = _db.Enderecos.Find(id);
+        if(endereco is null)
+            return RedirectToAction("Index");
+        return View(endereco);
+    }
+    [HttpPost]
+    public IActionResult Edit(int id, Endereco endereco)
+    {
+        var enderecooriginal = _db.Enderecos.Find(id);
+        if(enderecooriginal is null)
+            return RedirectToAction("Index");
+
+        if(!ModelState.IsValid)
+            return View(endereco);
+        
+        enderecooriginal.Logradouro = endereco.Logradouro;
+        enderecooriginal.Bairro = endereco.Bairro;
+        enderecooriginal.Numero = endereco.Numero;
+        enderecooriginal.Complemento = endereco.Complemento;
+        enderecooriginal.Cep = endereco.Cep;
+        enderecooriginal.Cidade = endereco.Cidade;
+        enderecooriginal.Estado = endereco.Estado;
+        enderecooriginal.Pais = endereco.Pais;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var endereco = _db.Enderecos.Find(id);
+        if(endereco is null)
+            return RedirectToAction("Index");
+        return View(endereco);
+        
+    }
+    [HttpPost]
+    public IActionResult ProcessDelete(Endereco endereco)
+    {
+        if(endereco is null)
+            return RedirectToAction("Index");
+        _db.Enderecos.Remove(endereco);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
