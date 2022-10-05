1c2d80ad820ff4e6dc3afb78a0b9f0bb49979787
Nícolas Bassini <nicolasbassini@hotmail.com>
2022-10-04 21:38:21 -0300

    melhoria script verificar senha

--- interfaces_novo/js/verificar_senha.js
+++ interfaces_novo/js/verificar_senha.js
@@ -1,3 +1,7 @@
+let botao = window.document.getElementById('proximo')
+botao.style.pointerEvents = 'none';
+botao.style.backgroundColor = "#0d3d57"
+
 function verificar()
 {
     let senha = window.document.getElementById('password').value
@@ -6,11 +10,17 @@ function verificar()
     
     if((senha == "") & (confirmar == "")){
         mensagem.innerHTML = ""
+        botao.style.pointerEvents = 'none'
+        botao.style.backgroundColor = "#0d3d57"
     } else if(senha == confirmar){
         mensagem.innerHTML = "Senhas conferem"
         mensagem.style.color = "green"
+        botao.style.pointerEvents = 'auto'
+        botao.style ='inital' 
     } else {
         mensagem.innerHTML = "Senhas não conferem"
         mensagem.style.color = "red"
+        botao.style.pointerEvents = 'none'
+        botao.style.backgroundColor = "#0d3d57"
     }
 }
